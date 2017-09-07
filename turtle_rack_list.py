1c8d24edd06207e360567f703522cd87b3cf4239
Yi Li <yil3@cisco.com>
2017-09-06 21:43:48 -0400

    added turtle race as a list

---
+++ turtle_rack_list.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+from random import randint
+
+turtle_colors = ['red', 'yellow', 'blue', 'green']
+turtles = []
+
+vPos = 100
+
+for turtlecolor in turtle_colors:
+    t = Turtle()
+    t.color(turtlecolor)
+    t.shape('turtle')
+    t.penup()
+    t.goto(-160, vPos)
+    t.pendown()
+
+    vPos -= 30
+
+    turtles.append(t)
+
+for movement in range (100):
+    for turtle in turtles:
+        turtle.forward(randint(1,5))
+
+wait = input("PRESS ENTER TO CONTINUE...")

e3a4de79e6fafdd8b4f0290f8a69fb3b2511d0da
Yi Li <yil3@cisco.com>
2017-09-14 21:37:45 -0400

    2.5

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ kitchen = Room("Kitchen")
 
 print("The room name is " + kitchen.name)
 print("The room description is " + str(kitchen.description))
+
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+
+kitchen.get_descprption()
+ktichen.describe()
